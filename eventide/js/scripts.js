521de7aeac4833826fbeb8149c476b7b521e7f3f
3pdt <yayayayaya91@gmail.com>
2017-09-30 01:10:02 +0300

    Commit Eventide

---
+++ eventide/js/scripts.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.collapse').on('show.bs.collapse', function(){
+    $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
+  });
+  $('.collapse').on('hide.bs.collapse', function(){
+    $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+  });
+  $('.form-sel').select2({
+    minimumResultsForSearch: Infinity
+  });
+  new WOW().init();
+});
